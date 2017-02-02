6ea95953d51e0c8dd5504256ab6693d8b9a5c0a7
sergey19941201 <koshelnick1994@mail.ru>
2017-02-02 13:42:42 +0200

    started

---
+++ Test.Droid/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Test.Droid
+{
+    [Activity(Label = "Test.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private FragmentManager fragmentManager;
+        private fragmentClasses.registrationFragment reg;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            reg = new fragmentClasses.registrationFragment();
+
+            reg.Show(fragmentManager, "fragmentManager");
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ Test.Droid/fragmentClasses/registrationFragment.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Test.Droid.fragmentClasses
+{
+    class registrationFragment : DialogFragment
+    {
+        private EditText eng1_ET, eng2_ET, eng3_ET;
+        private EditText rusEdText;
+        private Button dobavitBn, zakritFragmentbn;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.Inflate(Resource.Layout.registrationFragment, container, false);
+
+            dobavitBn = rootView.FindViewById<Button>(Resource.Id.dobavitBn);
+            zakritFragmentbn = rootView.FindViewById<Button>(Resource.Id.zakritFragmentBn);
+
+            eng1_ET = rootView.FindViewById<EditText>(Resource.Id.eng1_ET);
+            eng2_ET = rootView.FindViewById<EditText>(Resource.Id.eng2_ET);
+            eng3_ET = rootView.FindViewById<EditText>(Resource.Id.eng3_ET);
+            rusEdText = rootView.FindViewById<EditText>(Resource.Id.rusEditText);
+
+            dobavitBn.Click += DobavitBn_Click;
+            zakritFragmentbn.Click += ZakritFragmentbn_Click;
+
+            return rootView;
+        }
+
+        private void ZakritFragmentbn_Click(object sender, EventArgs e)
+        {
+            Dismiss();
+        }
+
+        private void DobavitBn_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(eng1_ET.Text) || String.IsNullOrEmpty(eng2_ET.Text) || String.IsNullOrEmpty(eng3_ET.Text) || String.IsNullOrEmpty(rusEdText.Text))
+            {
+                Toast.MakeText(this.Activity, "Çàïîëíèòå âñå ïîëÿ", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this.Activity, "Êàðòà äîáàâëåíà", ToastLength.Short).Show();
+
+                //Clearing EditTexts:
+                eng1_ET.Text = null;
+                eng2_ET.Text = null;
+                eng3_ET.Text = null;
+                rusEdText.Text = null;
+
+                Dismiss();
+            }
+        }
+    }
+}
